a5e02f1579bf16d9ac36d34387c78cef785f3a91
wong-bria <wong.bria@northeastern.edu>
2026-03-29 14:50:45 -0400

    feat: added routes and functions relating to Enrollments

--- kambaz/enrollments/dao.js
+++ kambaz/enrollments/dao.js
@@ -1,8 +1,26 @@
 import { v4 as uuidv4 } from "uuid"; 
+
 export default function EnrollmentsDao(db) { 
   function enrollUserInCourse(userId, courseId) { 
     const { enrollments } = db; 
     enrollments.push({ _id: uuidv4(), user: userId, course: courseId }); 
   } 
-  return { enrollUserInCourse }; 
+
+  function unenrollUserFromCourse(userId, courseId) {
+    const { enrollments } = db;
+    db.enrollments = enrollments.filter(
+      (e) => !(e.user === userId && e.course === courseId)
+    );
+  }
+
+  function findEnrollmentsForUser(userId) {
+    const { enrollments } = db;
+    return enrollments.filter((e) => e.user === userId);
+  }
+
+
+  return {  enrollUserInCourse,
+            unenrollUserFromCourse,
+            findEnrollmentsForUser,
+   }; 
 } 
